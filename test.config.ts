4ae5645e24cd9d18fdb74818f19e59a5a21767d9
aravindindapuram <aravind.indrapuram8@gmail.com>
2026-02-17 19:04:17 +0530

    ciiicooicco==

--- test.config.ts
+++ test.config.ts
@@ -1,15 +1,14 @@
 export class TestConfig {
+  //appUrl = "http://localhost/opencart/upload/"
+  appUrl = "https://naveenautomationlabs.com/opencart";
+  //appUrl="https://tutorialsninja.com/demo/"
 
-    appUrl = "http://localhost/opencart/upload/"
-    //appUrl="https://naveenautomationlabs.com/opencart"
-    //appUrl="https://tutorialsninja.com/demo/"
+  //Valid login credentials
+  email = "soumyapriya1298@gmail.com";
+  password = "Aravind@7";
 
-    //Valid login credentials
-    email = "soumyapriya1298@gmail.com"
-    password = "Aravind@7"
-
-    //product details
-    productName = "MacBook"
-    productQuantity = "2"
-    totalPrice = "$1,204.00"
+  //product details
+  productName = "MacBook";
+  productQuantity = "2";
+  totalPrice = "$1,204.00";
 }
